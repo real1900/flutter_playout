e62674cedc287f3bf0a567d68c519d25d47bf311
Suleman Imdad <suleman.imdad@me.com>
2024-06-08 19:16:14 -0700

    Update VideoPlayer.swift

--- ios/Classes/VideoPlayer.swift
+++ ios/Classes/VideoPlayer.swift
@@ -129,13 +129,15 @@ class VideoPlayer: NSObject, FlutterPlugin, FlutterStreamHandler, FlutterPlatfor
     }
     
     func setupPiP() {
-            guard let playerLayer = player?.currentItem?.playerLayer else {
-                print("Player layer is not available.")
-                return
-            }
-            
-            pictureInPictureController = AVPictureInPictureController(playerLayer: playerLayer)
-            pictureInPictureController?.delegate = self
+        guard let player = player else {
+                    print("AVPlayer instance is not available.")
+                    return
+                }
+                
+                let playerLayer = AVPlayerLayer(player: player)
+                
+                pictureInPictureController = AVPictureInPictureController(playerLayer: playerLayer)
+                pictureInPictureController?.delegate = self
         
         }
         
